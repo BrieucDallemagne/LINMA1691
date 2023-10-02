d26f74a978b3317debb7fb45282d57c573252476
BrieucDallemagne <brieuc1160@gmail.com>
2023-10-02 10:14:11 +0200

    petit q et tests

--- Devoir1/codes/for_student/solve.py
+++ Devoir1/codes/for_student/solve.py
@@ -13,24 +13,24 @@ def solve(adj) :
     N = len(adj) # number of nodes
     visited = [False]*N # is a node already visited?
     L = [] # list of node to process in the second step
-    q = deque() # queue of nodes to process with their associated status (i,False/True) i is the node index and True/False describes if we are appending the node to L or not when processing it
+    q1 = deque() # queue of nodes to process with their associated status (i,False/True) i is the node index and True/False describes if we are appending the node to L or not when processing it
 
     ### Step 1 : Depth-first search on adj
     for u in range(N) :
         if visited[u] : continue
         visited[u] = True
-        q.append((u,False))
-        while q :
-            x,to_append = q.pop()
+        q1.append((u,False))
+        while q1 :
+            x,to_append = q1.pop()
 
             if to_append:
                 L.append(x)
                 continue
 
-            q.append((x,True)) # When we will have process all the nodes linked to x, we can append x to L
+            q1.append((x,True)) # When we will have process all the nodes linked to x, we can append x to L
             for e in adj[x] : 
                 if not visited[e] :
-                    q.append((e,False))
+                    q1.append((e,False))
                     visited[e] = True
 
     ### reverse the list to obtain the post-order
@@ -40,19 +40,19 @@ def solve(adj) :
     ### Step 2 : Depth-first search on adj_T : find the strongly connected components
     assigned = [False]*N # is a node already visited?
     SCC = list() # List of the roots of the strongly connected components
-    q = deque()
+    q2 = deque()
 
     for u in L :
         if assigned[u] : continue
         else : SCC.append(u)
         assigned[u] = True
-        q.append(u)
-        while q :
-            x = q.pop()
+        q2.append(u)
+        while q2 :
+            x = q2.pop()
             for e in adj_T[x] :
                 if assigned[e] : continue
                 assigned[e] = True
-                q.append(e)
+                q2.append(e)
         
     # TODO : Find the kap at the roots of the SCC
     """
@@ -75,3 +75,7 @@ def transpose(adj) :
         for j in range (len(adj[i])) :
             adj_T[adj[i][j]].append(i)
     return adj_T
+
+# zone de test
+adj = [[1,2],[2],[],[],[4]]
+print(transpose(adj))
