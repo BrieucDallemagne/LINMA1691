c37cf85a86e8607b0c736a9602b294f4c84703dd
Brieuc Dallemagne <brieuc1160@gmail.com>
2023-11-18 04:18:19 +0100

    trop fatigué pour continuer

--- Devoir3/pathfinder_hw.py
+++ Devoir3/pathfinder_hw.py
@@ -1,28 +1,23 @@
 import copy
+from collections import deque
 
-"""
-Calcule un chemin eulérien dans graph et le retourne comme une liste de noeuds visités.
-Si aucun chemin eulérien n'existe, la fonction retourne None.
-L'argument graph ne doit pas être modifié lors de l'exécution de la fonction.
-"""
 def eulerian_path_finder(graph):
     new_graph = copy.deepcopy(graph)
     n = len(new_graph)
 
     eulerian_path = []
-    current_node = 0
-    lst = n*[False]
-    lst[current_node] = True
-    eulerian_path.append(current_node)
-    while len(eulerian_path) < n:
-        for i in range(n):
-            if new_graph[current_node][i] == 1 and lst[i] == False:
-                current_node = i
-                lst[i] = True
-                eulerian_path.append(current_node)
-                break
-        else:
-            return None
+    stack = deque()
+    
+    visited_edges = list()
+    for i in range(n):
+        visited_edges.append([False] * n)
+
+    "mettre l'algo ici j'y arrive pas"
+
+    
+    if len(eulerian_path) != len(graph) + 1:
+        return None
+    
 
 
     return eulerian_path
--- Devoir3/test_student.py
+++ Devoir3/test_student.py
@@ -1,4 +1,4 @@
-
+import pathfinder_hw as pf
 
 
 """
@@ -36,3 +36,34 @@ Writes a graph into file_name.
 def save_graph(file_name,graph):
     with open(file_name,'w') as file:
         file.write(from_adj_to_str(graph))
+
+"""
+test if the eulerian_path_finder function works correctly
+Pour vous aider, quelques graphes vous sont donnés dans des fichiers textes 2
+. Dans chacun des fichier, la
+première ligne annonce le nombre de noeuds et chacune des lignes suivantes donne la liste d’adjacence du
+noeud correspondant. Une fonction de lecture des fichier test donnant un input utilisable vous est également
+fourni. Les solutions n’étant pas uniques pour un graphe donné, il ne tient qu’à vous de vérifier les réponses
+de votre fonction.
+"""
+def test_eulerian_path_finder():
+    graph = load_graph("test_01.txt")
+    print(pf.eulerian_path_finder(graph))
+    graph = load_graph("test_02.txt")
+    print(pf.eulerian_path_finder(graph))
+    graph = load_graph("test_03.txt")
+    print(pf.eulerian_path_finder(graph))
+    graph = load_graph("test_04.txt")
+    print(pf.eulerian_path_finder(graph))
+    graph = load_graph("test_05.txt")
+    print(pf.eulerian_path_finder(graph))
+    graph = load_graph("test_06.txt")
+    print(pf.eulerian_path_finder(graph))
+    graph = load_graph("test_07.txt")
+    print(pf.eulerian_path_finder(graph))
+    graph = load_graph("test_08.txt")
+    print(pf.eulerian_path_finder(graph))
+    graph = load_graph("test_09.txt")
+    print(pf.eulerian_path_finder(graph))
+
+test_eulerian_path_finder()
